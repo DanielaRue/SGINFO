f987a0eb6761ba06a12be74822a91cb1b91765ed
raymansell <61888240+raymansell@users.noreply.github.com>
2020-12-14 23:23:19 -0500

    consuming 'productos' endpoint

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -8,13 +8,34 @@
         </b-navbar-brand>
       </b-navbar>
     </div>
-    <hr/>
+    <hr />
+    <div>
+      <b-table striped hover :items="productos"></b-table>
+    </div>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   name: "Productos",
+  data() {
+    return {
+      productos: []
+    };
+  },
+  created() {
+    let self = this;
+    this.getAllProducts()
+      .then(result => {
+        self.productos = result.data;
+      })
+      .catch(error => alert("Error de Servidor"));
+  },
+  methods: {
+    getAllProducts() {
+      return axios.get("http://127.0.0.1:8000/productos");
+    }
+  }
 };
 </script>
-<style>
-</style>
+<style></style>
