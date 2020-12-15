ec43e2097a39bc0bc0d90171f952c8c19b9b7cfe
raymansell <61888240+raymansell@users.noreply.github.com>
2020-12-15 15:04:45 -0500

    new product submission form

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <div class="d-flex">
-
       <div class="shadow-sm p-1 rounded sidebar">
         <div>
           <div>
@@ -28,66 +27,63 @@
           <router-view></router-view>
         </div>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    components: {},
-    methods: {
-
-      client: function(){
-        if(this.$route.name != "clientes"){
-          let username = localStorage.getItem("current_username")
-          this.$router.push({name: "clientes"})
-        }
-      },
-      produc: function(){
-        if(this.$route.name != "productos"){
-          let username = localStorage.getItem("current_username")
-          this.$router.push({name: "productos"})
-        }
-      },
-      repor: function(){
-        if(this.$route.name != "reporte"){
-          let username = localStorage.getItem("current_username")
-          this.$router.push({name: "reporte"})
-        }
+export default {
+  name: "App",
+  components: {},
+  methods: {
+    client: function() {
+      if (this.$route.name != "clientes") {
+        let username = localStorage.getItem("current_username");
+        this.$router.push({ name: "clientes" });
+      }
+    },
+    produc: function() {
+      if (this.$route.name != "productos") {
+        let username = localStorage.getItem("current_username");
+        this.$router.push({ name: "productos" });
+      }
+    },
+    repor: function() {
+      if (this.$route.name != "reporte") {
+        let username = localStorage.getItem("current_username");
+        this.$router.push({ name: "reporte" });
       }
     }
   }
+};
 </script>
 
 <style>
-  body{
-    margin: 0 0 0 0;
-  }
-  .d-flex{
-    margin:0 0 0 0;
-    display: flex;
-  }
-  .sidebar{
-    width: 210px;
-    height: 100vh;
-    background-color: #031926;
-    color:#f9f9fa;
-  }
-  .btn-secondary:hover {
-    color: #fff;
-    background-color:#254D58;
-    border-color:#254D58;
-    width: 200px;
-  }
-  .menu{
-    width: 200px;
-    background-color: #031926;
-    border-color: #031926;
-  }
-  .custom{
-    flex-grow: 1;
-  }
-
+body {
+  margin: 0 0 0 0;
+}
+.d-flex {
+  margin: 0 0 0 0;
+  display: flex;
+}
+.sidebar {
+  width: 210px;
+  min-height: 100vh;
+  background-color: #031926;
+  color: #f9f9fa;
+}
+.btn-secondary:hover {
+  color: #fff;
+  background-color: #254d58;
+  border-color: #254d58;
+  width: 200px;
+}
+.menu {
+  width: 200px;
+  background-color: #031926;
+  border-color: #031926;
+}
+.custom {
+  flex-grow: 1;
+}
 </style>
